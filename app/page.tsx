54bce967028c73f187af94c540e8da96fd3744a6
Luca Kursawe <30121038+lucakursawe@users.noreply.github.com>
2025-05-20 22:29:25 +0200

    changed wording

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,7 @@ export default function Page() {
         Luca Kursawe
       </p>
       <p className="mb-4">
-        {` I'm a Creative Technologist and Computer Science student at RH Köln, working across web development, design, and data analysis.
-I enjoy solving real problems in creative, visual ways — whether that's building frontend projects, analyzing data, or making photography and video content.`}
+        {`Creative Technologist and Computer Science student at RH Köln, working across web development, design, and data analysis. I enjoy solving real problems in creative, visual ways — whether that's building frontend projects, analyzing data, or making photography and video content.`}
       </p>
       <div className="my-8">
         <BlogPosts />
